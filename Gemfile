8b5a0bfc2317198501c5f4a2720b5d892201fbb2
chriselevate <coates.c@hotmail.com>
2016-05-03 22:13:43 +0100

    completed up to 13

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'rspec'
+gem 'capybara'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+gem 'database_cleaner'
---
+++ app/app.rb
@@ -0,0 +1,24 @@
+ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+require_relative 'models/link'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/links' do
+    @links = Link.all
+    erb(:'links/index')
+  end
+
+  get '/links/new' do
+    erb(:'links/new')
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
