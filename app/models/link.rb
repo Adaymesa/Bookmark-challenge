431a8ba666190f4f449a52b73e9c41476d2baf14
Aday Mesa <adaymesagc@gmail.com>
2016-05-04 17:20:10 +0100

    URL back

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,6 @@ class Link
 end
 require 'data_mapper'
 
-DataMapper.setup(:default, ENV['postgres://oxtufilzwfrzpw:XjIOlyydVY_EnvUOxigwPXYNv6@ec2-107-20-174-127.compute-1.amazonaws.com:5432/d85t2n534b7lea'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
