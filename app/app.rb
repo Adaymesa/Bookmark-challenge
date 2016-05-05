6601199ca926e19a505d3feb37e0cd14adbb7dae
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-05 16:12:54 +0100

    Challenge 16: refactored to walkthrough example

--- app/app.rb
+++ app/app.rb
@@ -21,9 +21,10 @@ class BookmarkManager < Sinatra::Base
     redirect to('/links')
   end
 
-  get '/tags/bubbles' do
-    @links = Link.all
-    erb(:'/tags/bubbles')
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb(:'links/index')
   end
 
   # start the server if ruby file executed directly
