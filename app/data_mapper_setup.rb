3620d5b4897bf538be9b285573b4f0defb543d2c
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-05 16:13:58 +0100

    complete step 18, error fixed relation "users" does not exist
    The error was fixed adding in data_mapper_setup.rb the line
    DataMapper.auto_upgrade!
    
    and that fixed the error, so that implies that the rakefile proposed
    by MA is not correct.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,3 +7,4 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
+DataMapper.auto_upgrade!
