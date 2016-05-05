0f5bee7b5daac775bb049678d551454fd77cbecd
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-06 00:44:23 +0100

    step 23, flash for incorrect login does not work

--- app/app.rb
+++ app/app.rb
@@ -19,7 +19,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/' do
-    redirect '/links'
+    "Hello"
   end
 
   get '/links' do
@@ -69,12 +69,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect '/links'
     else
-      flash.now[:sessions] = ['The email or  password is incorrect']
+      flash.now[:sessions] = ['The email or password is incorrect']
+      # redirect "/"  # why the flash does not work???
       erb :'sessions/new'
     end
   end
